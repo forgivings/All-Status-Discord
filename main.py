a326375759066ed750897eea6a5b92543b2ff69f
Forgiving <forgiving@forgiving.ml>
2020-09-18 18:54:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from discord.ext import commands
 #from discord.utils import get
 
 client = commands.Bot(
-  command_prefix = "2"
+  command_prefix = "ANY PREFIX YOU WANT"
 )
 @client.event
 async def on_ready():
     print('bot success')
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="you 24/7"))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="YOUR WATCHING HERE"))
+    #await bot.change_presence(activity=discord.Game(name="YOUR GAME HERE"))
+    #await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="YOUR SONG HERE"))
+    #await bot.change_presence(activity=discord.Streaming(name="STREAM TITLE HERE", url="TWITCH URL HERE"))
+    #Only one at a time and only delete the '#' to change it (Fun Fact: Watching status is only for bots right now, but users can use it too!)
 
 
 @client.command()
